bbe233379b4acd40cac00a47c2c6d6dfb2f82355
morteako <morteako@student.matnat.uio.no>
2018-12-06 02:06:00 +0100

    day 5

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,10 +1,25 @@
 module Day05 where
 
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict ((!),(!?))
-import Control.Lens
+import Data.Char (toLower)
+import Control.Arrow ((&&&))
 
+
+react :: String -> String
+react (x:y:xs) 
+    |   toLower x == toLower y && x /= y = react xs
+    |   otherwise = x:react (y:xs)
+react xs = xs
+
+--Should have used foldr and do everything in one pass instead of finding the fix point
+-- -1 because of newline at the end
+fullyReact :: String -> Int
+fullyReact = subtract 1 . length . until ((==) <*> react) react
+    
+fullyReactALittleFaster = subtract 1 . length . fst . head . filter (uncurry (==)) . (zip <*> tail) . iterate react
+
+solveA = fullyReact 
+
+solveB xs =  minimum [fullyReact $ filter ((char /=) . toLower) xs| char <- ['a'..'z']]
+   
 main :: IO ()
-main = do
-    contents <- readFile "05.txt"
-    print "ok"
+main = readFile "data/day5.txt" >>= print . (solveA &&& solveB)
