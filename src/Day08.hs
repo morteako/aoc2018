08e171f94e6bd11e96fa86b8b2d2fc8c1becedab
morteako <morteako@student.matnat.uio.no>
2018-12-09 15:19:37 +0100

    day 8 and day 9

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,10 +1,51 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day08 where
 
+import Control.Monad.State.Strict
 import qualified Data.Map.Strict as Map
-import Data.Map.Strict ((!),(!?))
+import Data.Map.Strict (Map)
 import Control.Lens
+import Control.Lens.Lens
+import Control.Lens.TH
+import Data.Maybe (mapMaybe)
+
+data T = T {_counter :: Int, _restT :: [Int]}
+makeLenses ''T
+
+data Tree = Tree (Map Int Tree) [Int]
+
+
+findSum :: Tree -> Int
+findSum (Tree m meta) = sum meta + sum (Map.map findSum m)
+
+rootVal :: Tree -> Int
+rootVal (Tree m meta) 
+    | Map.null m = sum meta
+    | otherwise = sum $ fmap rootVal $ mapMaybe (`Map.lookup` m) meta
+
+solve :: String -> (Int,Int)
+solve xs = (findSum tre,rootVal tre)
+    where 
+        tre :: Tree
+        tre = go . map (read::String -> Int) . words $ xs
+        
+        go (c:metas:rest) = evalState (createTree 1 c metas) (T 0 rest)
+        
+        createTree :: Int -> Int -> Int -> State T Tree
+        createTree childCounter 0 m = do
+            -- <<%= updates the state, but returns the old value
+            rest <- restT <<%= drop m
+            return $ Tree Map.empty $ take m rest
+        createTree childCounter c m = do
+            (childs : metas : rest) <- restT <<%= drop 2
+            childTree <- createTree 1 childs metas
+            Tree restOfCurrent metas <- createTree (childCounter+1) (c-1) m
+            return $ Tree (Map.insert childCounter childTree restOfCurrent) metas 
 
 main :: IO ()
-main = do
-    contents <- readFile "08.txt"
-    print "ok"
+main = readFile "data/day8.txt" >>= print . solve 
+    
+    
+
+
--- src/Day09.hs
+++ src/Day09.hs
@@ -1,10 +1,43 @@
 module Day09 where
 
 import qualified Data.Map.Strict as Map
-import Data.Map.Strict ((!),(!?))
+import Data.Map.Strict (Map)
 import Control.Lens
+import qualified Data.Sequence as Seq
+import Debug.Trace
+
+maxPlayers = 493
+maxMarble = 71863*100
+
+
+next p = mod p maxPlayers + 1
+
+getIndex i seq 
+    | i == Seq.length seq = i
+    | otherwise =  i `mod` Seq.length seq
+
+doStuff :: Map Int Int -> Seq.Seq Int -> Int -> Int -> Int -> Map Int Int
+doStuff players marbles pc mc curMarbleI 
+    | mc == maxMarble + 1 = players
+    | mod mc 23 == 0 =
+        let
+            p = Map.adjust (+ (mc + prevCurMarble)) pc players
+            cur7 = getIndex (curMarbleI - 7) marbles
+            prevCurMarble = Seq.index marbles cur7
+            marDel = Seq.deleteAt cur7 marbles
+            newCurMarble = Seq.index marDel cur7
+        in
+            doStuff p marDel (next pc) (mc+1) cur7
+    | otherwise = 
+        let
+            newCurMarbleI = (getIndex (curMarbleI + 2) marbles)
+            s = Seq.insertAt newCurMarbleI mc marbles  
+        in 
+            doStuff players s (next pc) (mc+1) newCurMarbleI
+        
+
+start = doStuff (Map.fromList $ zip [1..maxPlayers] (repeat 0)) (Seq.fromList [0]) 1 1 0 
+
 
 main :: IO ()
-main = do
-    contents <- readFile "09.txt"
-    print "ok"
+main = print $ maximum start
